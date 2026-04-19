fdc96736324856f1715a6d90d67494485c112968
Kunal Deo <kunaldeo001@gmail.com>
2026-04-19 22:35:11 +0530

    refactor: generic formatTime function in comingSoon.js

--- js/comingSoon.js
+++ js/comingSoon.js
@@ -6,20 +6,8 @@ var x = setInterval(function(){
     var hours = Math.floor((distance%(1000*60*60*24)) / (1000*60*60));
     var minutes = Math.floor((distance%(1000*60*60))/(1000*60));
     var seconds = Math.floor((distance%(1000*60)) / 1000);
-    function formatTime(days){
-        return days<10 ?(`0${days}`):days;
-    }  
-
-    function formatTime(hours){
-        return hours<10 ?(`0${hours}`):hours;
-    }    
-    
-    function formatTime(minutes){
-        return minutes<10 ?(`0${minutes}`):minutes;
-    }    
-    
-    function formatTime(seconds){
-        return seconds<10 ?(`0${seconds}`):seconds;
+    function formatTime(time){
+        return time<10 ?(`0${time}`):time;
     }
     document.getElementById("launch").innerHTML = formatTime(days)+ " Days &nbsp;&nbsp;"+formatTime(hours)+" Hours &nbsp;&nbsp;"+formatTime(minutes)+" Minutes &nbsp;&nbsp;"+formatTime(seconds)+ " Seconds To Go...";
     if (distance < 0){
